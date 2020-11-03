dc8d061af80bce82c6005e2fd8720f3f2d216834
Stepan Legachev <gitlog@sib.li>
2020-11-03 20:09:08 +0300

    feat(dev): add rollup watch mode config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,38 @@
 import typescript from '@rollup/plugin-typescript';
+import {terser} from 'rollup-plugin-terser';
+import run from '@rollup/plugin-run';
+
+const dev = process.env.ROLLUP_WATCH === 'true';
 
 export default {
   input: 'src/main.ts',
 
-  output: {
+  output: [{
     dir: 'dist',
-    format: 'es'
-  },
+    format: 'es',
+    // @fixme use terser for production only
+    plugins: [terser()],
+    sourcemap: true
+  }],
 
-  plugins: [typescript()]
+  external: [
+    'path',
+    'url',
+    'fastify',
+    'fastify-static'
+  ],
+
+  plugins: [
+    typescript(),
+    dev && run({
+      execArgv: ['-r', 'source-map-support/register']
+    })
+  ],
+
+  watch: {
+    chokidar: true,
+    // include and exclude govern which files to watch. by
+    // default, all dependencies will be watched
+    exclude: ['node_modules/**']
+  }
 };
