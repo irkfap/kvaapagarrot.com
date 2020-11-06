8eb6f2dad7073413692d782801e40a61b7f0ec10
Stepan Legachev <gitlog@sib.li>
2020-11-07 00:48:32 +0300

    feat(gae): automate static handlers, expand concept

--- scripts/make-static-handlers.ts
+++ scripts/make-static-handlers.ts
@@ -1,18 +1,48 @@
-import * as path from 'path';
-import {fileURLToPath} from 'url';
-import glob from 'tiny-glob';
+import {join as pathJoin} from 'path';
+import glob from 'fast-glob';
+import {Options} from 'fast-glob/out/settings';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const STATIC_DIR = pathJoin(process.cwd(), 'public');
 
-const STATIC_DIR = path.join(__dirname, '..', '..', 'public');
+const ADD_MIME_TYPES = {
+  'site.webmanifest' : {mime_type: 'application/json', expiration: '10m'},
+  '*.ico' : 'image/x-icon',
+  'robots.txt': {expiration: '1s'},
+  'sitemap.xml': {expiration: '1s'},
+  '*.eot' : 'application/vnd.ms-fontobject',
+  '*.ttf' : 'font/ttf',
+  '*.woff' : 'font/woff',
+  '*.woff2' : 'font/woff2',
+};
+
+const IGNORE_FILES = [
+  '**/.DS_Store',
+  '**/LICENSE.txt',
+  '**/config.json'
+];
+
+const GLOB_OPTIONS: Options = {
+  braceExpansion: false,
+  caseSensitiveMatch: false,
+  cwd: STATIC_DIR,
+  extglob: false,
+  followSymbolicLinks: false,
+  ignore: IGNORE_FILES,
+  onlyFiles: true,
+};
 
 (async () => {
 
-  const templates = await glob(`**/*.*`, {
-    cwd: STATIC_DIR,
+  // const pattern = '**/('+Object.keys(ADD_MIME_TYPES).join('|')+')';
+  const pattern = Object.keys(ADD_MIME_TYPES).map(k => `**/${k}`);
+
+  const filesKnown = await glob(pattern , GLOB_OPTIONS);
+
+  const filesAll = await glob('**/*.*' , {
+    ...GLOB_OPTIONS,
+    ignore: IGNORE_FILES.concat(pattern),
   });
 
-  console.log(templates);
+  console.log(filesKnown.concat(filesAll));
 
 })();
